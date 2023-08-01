91d80ea4be14a5450001ef8c281d40ac1b8764e3
Half212 <aquiles.filho250@gmail.com>
2023-08-01 12:00:04 -0300

    sobreescrevendo metodos any

--- src/br/com/alura/bytebank/Main.kt
+++ src/br/com/alura/bytebank/Main.kt
@@ -2,10 +2,18 @@ import br.com.alura.bytebank.model.Endereco
 import br.com.alura.bytebank.test.testaFuncionarios
 
 fun main() {
-    val endereco = Endereco(logradouro = "Rua vergueiro", complemento = "Alura")
-    val enderecoNovo = Endereco(bairro = "Vila Mariana", numero = 1000)
+    val endereco = Endereco(
+        logradouro = "Rua vergueiro",
+        complemento = "Alura",
+        cep = "00000-000"
+    )
+    val enderecoNovo = Endereco(
+        bairro = "Vila Mariana",
+        numero = 1000,
+        cep = "00000-000"
+    )
 
-    println(endereco.equals(endereco))
+    println(endereco.equals(enderecoNovo))
 
     println(endereco.hashCode())
     println(enderecoNovo.hashCode())
